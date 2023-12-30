2826666729aeb1624244096877feaa367c3ad20c
eskokado <eskokado@gmail.com>
2023-12-30 07:59:38 -0300

    feat: impl shared toolbar-navigation

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -6,28 +6,34 @@ import { RouterModule } from '@angular/router';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
+import { CardModule } from 'primeng/card';
+import { ToastModule } from 'primeng/toast';
+import { ChartModule } from 'primeng/chart'
+import { MessageService } from 'primeng/api';
+
+import { CookieService } from 'ngx-cookie-service';
 
 import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.component';
 import { DASHBOARD_ROUTES } from './dashboard.routing';
-import { CardModule } from 'primeng/card';
-import { ToastModule } from 'primeng/toast';
-import { MessageService } from 'primeng/api';
-import { CookieService } from 'ngx-cookie-service';
+import { SharedModule } from 'src/app/shared/shared.module'
 
 @NgModule({
-  declarations: [DashboardHomeComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forChild(DASHBOARD_ROUTES),
-    // PrimeNg
-    SidebarModule,
-    ButtonModule,
-    ToolbarModule,
-    CardModule,
-    ToastModule,
-  ],
-  providers: [MessageService, CookieService],
+    declarations: [DashboardHomeComponent],
+    providers: [MessageService, CookieService],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forChild(DASHBOARD_ROUTES),
+        // PrimeNg
+        SidebarModule,
+        ButtonModule,
+        ToolbarModule,
+        CardModule,
+        ToastModule,
+        ChartModule,
+        // Shared
+        SharedModule,
+    ]
 })
 export class DashboardModule {}
